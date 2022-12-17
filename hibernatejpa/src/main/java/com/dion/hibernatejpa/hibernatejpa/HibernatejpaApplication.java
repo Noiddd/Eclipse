3cd98c9124e5b6d12ac4ee36041e89c57383143c
Noiddd <dionang11@gmail.com>
2022-12-17 12:02:38 +0800

    first commit

---
+++ hibernatejpa/src/main/java/com/dion/hibernatejpa/hibernatejpa/HibernatejpaApplication.java
@@ -0,0 +1,13 @@
+package com.dion.hibernatejpa.hibernatejpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernatejpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernatejpaApplication.class, args);
+		System.out.println("hello");
+	}
+}
